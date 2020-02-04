e9d64509fb5c223357055f552f5d6b534697cff2
wesley <ewesleymanon@gmail.com>
2020-02-04 11:53:14 +0800

    ade added animation on nav toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,21 +4,16 @@ $(function(){
   $(".toggle").click(function(){
     flag= !flag;
     if(flag) {
-      $('body').css('overflow', 'hidden');
+      $(".main-nav").addClass('nav-responsive');
       $('.toggle-icon').addClass('toggle-icon--active');
-      $('.main-nav').addClass('nav-responsive');
-      $('.main-nav').css('height', '100%');
-      // $("#menu").slideDown(500);
-      $('#menu').css('display', 'block');
-
+      $('body').css('overflow', 'hidden');
+      $('#menu').slideDown(500);
     }
     else {
-      $('.main-nav').removeClass('nav-responsive');
+      $(".main-nav").removeClass('nav-responsive');
       $('.toggle-icon').removeClass('toggle-icon--active');
       $('body').css('overflow', 'auto');
-      // $("#main-nav").slideUp(500);
-      $('.main-nav').css('height', '-100%');
-      // $('#menu').css('display', 'none');
+      $('#menu').css('display', 'none');
     }
   });
 });
@@ -26,17 +21,16 @@ $(function(){
 function mdCheck(md) {
   if(md.matches) {
     $(".toggle").css("pointer-events","auto");
-    $('#menu').css('display', 'none')
+    $('#menu').css('display', 'none');
     if(flag) {
       flag = !flag;
     }
   }
   else {
-    $('#menu').css('display', 'block')
     $(".toggle").css("pointer-events","none");
+    $(".main-nav").removeClass('nav-responsive')
+    $('#menu').css('display', 'flex')
     $('.toggle-icon').removeClass('toggle-icon--active');
-    $('#menu').removeClass('toggled');
-    $('.main-nav').removeClass('nav-responsive');
     $('body').css('overflow', 'auto');
   }
 }
